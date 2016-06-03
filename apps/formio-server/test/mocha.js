4515204d66de71c21bd72d5a6a4d949641bcc69e
Randall Knutson <randall@form.io>
2016-06-03 15:12:27 -0500

    Wrap before in promise to fix timing issue.

--- apps/formio-server/test/mocha.js
+++ apps/formio-server/test/mocha.js
@@ -865,12 +865,14 @@ describe('Bootstrap', function() {
 
     describe('Install Tests', function() {
       before(function(done) {
-        // Ensure the admin email and password are set for install process.
-        process.env.ADMIN_EMAIL = 'test@example.com';
-        process.env.ADMIN_PASS = 'password';
-        // Clear the database, reset the schema and perform a fresh install.
-        app.formio.db.dropDatabase();
-        require('../install')(app.formio, done);
+        return ready.then(function() {
+          // Ensure the admin email and password are set for install process.
+          process.env.ADMIN_EMAIL = 'test@example.com';
+          process.env.ADMIN_PASS = 'password';
+          // Clear the database, reset the schema and perform a fresh install.
+          app.formio.db.dropDatabase();
+          require('../install')(app.formio, done);
+        });
       });
 
       require('./install')(app, template, hook);
