f1502d7f0180aa202d0375663e81caa68aafcdf4
Zack Urben <zackurben@gmail.com>
2016-08-02 17:33:10 -0500

    Adding initial atlassian oauth config stuff.

--- apps/formio-server/src/actions/atlassian/util.js
+++ apps/formio-server/src/actions/atlassian/util.js
@@ -6,10 +6,13 @@ var debug = {
   createIssue: require('debug')('formio:atlassian:createIssue'),
   updateIssue: require('debug')('formio:atlassian:updateIssue'),
   deleteIssue: require('debug')('formio:atlassian:deleteIssue'),
-  checkSettings: require('debug')('formio:atlassian:checkSettings')
+  checkSettings: require('debug')('formio:atlassian:checkSettings'),
+  checkOAuth: require('debug')('formio:atlassian:checkOAuth')
 };
 
 module.exports = function(router) {
+  var formio = router.formio;
+
   /**
    * Check to see if the connection settings are present.
    *
@@ -43,6 +46,43 @@ module.exports = function(router) {
 
     return true;
   };
+
+  var checkOAuth = function(settings) {
+    if (!settings) {
+      debug.checkOAuth('No OAuth settings found');
+      return false;
+    }
+
+    if (!_.has(settings, 'oauth.consumer_key')) {
+      debug.checkOAuth('No OAuth consumer key is configured');
+      return false;
+    }
+
+    if (!_.has(settings, 'oauth.private_key')) {
+      debug.checkOAuth('No OAuth private key is configured');
+      return false;
+    }
+
+    return true;
+  };
+
+  router.use('/atlassian/oauth/authorize', function(req, res, next) {
+    if (!checkOAuth(_.get(req, 'body'))) {
+      return res.sendStatus(404);
+    }
+
+    JiraClient.oauth_util.getAuthorizeURL({
+      consumer_key: _.get(req, 'body.oauth.consumer_key'),
+      private_key: _.get(req, 'body.oauth.consumer_key')
+    }, function(err, handshake) {
+      if (err) {
+        return res.sendStatus(404);
+      }
+
+      // Start the dance with the handshake.
+      return res.json(handshake);
+    });
+  });
 
   return {
     getJira: function(settings) {
