johnformio/server-repo

commit 20e932ade76ff903492b48012d3b004513075486

Cleaned up actions and introdcued an initialize method.

apps/formio/src/actions/actions.js
@@ -4,10 +4,6 @@ var Resource = require('resourcejs');
 var async = require('async');
 var mongoose = require('mongoose');
 var _ = require('lodash');
-var debug = {
-  search: require('debug')('formio:action#search'),
-  execute: require('debug')('formio:action#execute')
-};
 
 /**
  * The ActionIndex export.
@@ -46,51 +42,115 @@ module.exports = function(router) {
     model: mongoose.model('action', hook.alter('actionSchema', Action.schema)),
 
     /**
-     * Allow search capabilities for finding an Action.
+     * Load all actions for a provided form.
      *
-     * @param handler
-     * @param method
-     * @param form
+     * @param req
      * @param next
+     * @returns {*}
      */
-    search: function(handler, method, form, next) {
+    loadActions: function(req, res, next) {
+      if (!req.actions) {
+        req.actions = {};
+      }
+
+      var form = req.formId;
       if (!form) {
         return next();
       }
 
-      async.waterfall([
-        function queryMongo(callback) {
-          this.model
-            .find({
-              handler: handler,
-              method: method,
-              form: form,
-              deleted: {$eq: null}
-            })
-            .sort('-priority')
-            .exec(function(err, rows) {
-              if (err) {
-                return callback(err);
-              }
+      // Use cache if it is available.
+      if (req.actions && req.actions[form]) {
+        return next(null, req.actions[form]);
+      }
 
-              // Execute the callback.
-              callback(null, rows);
-            });
-        }.bind(this),
-        function processResults(rows, callback) {
-          rows = rows || [];
-
-          // Execute the callback.
-          callback(null, rows);
-        }.bind(this)
-      ], function(err, results) {
+      // Find the actions associated with this form.
+      this.model.find({
+        form: form,
+        deleted: {$eq: null}
+      })
+      .sort('-priority')
+      .exec(function(err, result) {
         if (err) {
-          debug.search(err);
           return next(err);
         }
 
-        next(null, results);
-      });
+        // Iterate through all of the actions and load them.
+        var actions = [];
+        _.each(result, function(action) {
+          if (!this.actions.hasOwnProperty(action.name)) {
+            return;
+          }
+
+          // Create the action class.
+          var ActionClass = this.actions[action.name];
+          actions.push(new ActionClass(action, req, res));
+        }.bind(this));
+
+        req.actions[form] = actions;
+        return next(null, actions);
+      }.bind(this));
+    },
+
+    /**
+     * Find an action within the available actions for this form.
+     *
+     * @param handler
+     * @param method
+     * @param req
+     * @param next
+     */
+    search: function(handler, method, req, res, next) {
+      if (!req.formId) {
+        return next(null, []);
+      }
+
+      // Make sure we have actions attached to the request.
+      if (req.actions) {
+        var actions = [];
+        _.each(req.actions[req.formId], function(action) {
+          if (
+            (!handler || action.handler.indexOf(handler) !== -1) &&
+            (!method || action.method.indexOf(method) !== -1)
+          ) {
+            actions.push(action);
+          }
+        });
+        return next(null, actions);
+      }
+      else {
+        // Load the actions.
+        this.loadActions(req, res, function(err) {
+          if (err) {
+            return next(err);
+          }
+          this.search(handler, method, req, res, next);
+        }.bind(this));
+      }
+    },
+
+    /**
+     * Load an initialize all actions for this form.
+     *
+     * @param req
+     * @param res
+     * @param next
+     */
+    initialize: function(method, req, res, next) {
+      this.search(null, method, req, res, function(err, actions) {
+        if (err) {
+          return next(err);
+        }
+
+        // Iterate through each action.
+        async.forEachOf(actions, function(action, index, done) {
+          if (actions[index].initialize) {
+            actions[index].initialize(req, res, done);
+          }
+          else {
+            done();
+          }
+        }.bind(this), next);
+      }.bind(this));
     },
 
     /**
@@ -103,70 +163,33 @@ module.exports = function(router) {
      * @param next
      */
     execute: function(handler, method, req, res, next) {
-      if (!req.formId) {
-        return next();
-      }
-
-      async.waterfall([
-        function actionSearch(callback) {
-          this.search(handler, method, req.formId, function(err, result) {
-            if (err) {
-              return callback(err);
-            }
-
-            callback(null, result);
-          });
-        }.bind(this),
-        function actionExecute(result, callback) {
-          if (!result || !result.length) {
-            return callback(null, null);
-          }
-
-          // The actions to execute.
-          var actions = [];
-
-          // Iterate over each action.
-          _.each(result, function(action) {
-            if (!action.name) {
-              return callback(new Error('No action was provided'));
-            }
-            if (!this.actions.hasOwnProperty(action.name)) {
-              return callback(new Error('Action not found.'));
-            }
-
-            var ActionClass = this.actions[action.name];
-            if (action.condition && action.condition.field) {
-              // If condition is set, make sure it matches.
-              if (
-                (req.body.data[action.condition.field] === action.condition.value) ===
-                (action.condition.eq === 'equals')
-              ) {
-                actions.push(new ActionClass(action, req, res));
-              }
-            }
-            else {
-              actions.push(new ActionClass(action, req, res));
-            }
-          }.bind(this));
-
-          // Iterate and execute each action.
-          async.eachSeries(actions, function(action, cb) {
-            action.resolve(handler, method, req, res, cb);
-          }, function(err) {
-            if (err) {
-              return callback(err);
-            }
-
-            callback();
-          });
-        }.bind(this)
-      ], function(err) {
+      // Find the available actions.
+      this.search(handler, method, req, res, function(err, actions) {
         if (err) {
-          debug.execute(err);
           return next(err);
         }
 
-        next();
+        // Iterate and execute each action.
+        async.eachSeries(actions, function(action, cb) {
+          // See if a condition is not established within the action.
+          if (
+            action.condition &&
+            action.condition.field &&
+            (action.condition.eq === 'equals') &&
+            (req.body.data[action.condition.field] !== action.condition.value)
+          ) {
+            return cb();
+          }
+
+          // Resolve the action.
+          action.resolve(handler, method, req, res, cb);
+        }, function(err) {
+          if (err) {
+            return next(err);
+          }
+
+          next();
+        });
       });
     }
   };

apps/formio/src/middleware/submissionHandler.js
@@ -3,10 +3,6 @@
 var _ = require('lodash');
 var async = require('async');
 var deleteProp = require('delete-property');
-var debug = {
-  before: require('debug')('formio:middleware:submissionHandler#before'),
-  after: require('debug')('formio:middleware:submissionHandler#after')
-};
 var util = require('../util/util');
 var Validator = require('../resources/Validator');
 
@@ -76,6 +72,7 @@ module.exports = function(router, resourceName, resourceId) {
         }
 
         req.currentForm = form;
+        req.flattenedComponents = util.flattenComponents(form.components);
         done();
       });
     };
@@ -123,11 +120,26 @@ module.exports = function(router, resourceName, resourceId) {
         // Allow them to alter the body.
         req.body = hook.alter('submissionRequest', req.body);
 
-        // Create a submission object.
+        // Assign submission data to the request body.
+        req.submission = req.submission || {data: {}};
+        req.body.data = _.assign(req.body.data, req.submission.data);
+
+        // Clone the submission to the real value of the request body.
         req.submission = _.clone(req.body, true);
       }
 
       done();
+    };
+
+    /**
+     * Initialize the actions.
+     *
+     * @param req
+     * @param res
+     * @param done
+     */
+    var initializeActions = function(req, res, done) {
+      router.formio.actions.initialize(method.name, req, res, done);
     };
 
     /**
@@ -164,80 +176,94 @@ module.exports = function(router, resourceName, resourceId) {
      * @param req
      * @param done
      */
-    var executeHandlers = function(req, res, done) {
+    var executeFieldActionHandlers = function(req, res, done) {
       // Iterate through each component and allow them to alter the query.
-      var flattenedComponents = util.flattenComponents(req.currentForm.components);
-      async.eachSeries(flattenedComponents, function(component, done) {
+      async.eachSeries(req.flattenedComponents, function(component, then) {
         if (
           req.body &&
           component.hasOwnProperty('persistent') &&
           !component.persistent
         ) {
-          debug.before('Removing non-persistent field:', component.key);
-          // Delete the value from the body if it isn't supposed to be persistent.
           deleteProp('data.' + util.getSubmissionKey(component.key))(req.body);
         }
 
         // Execute the field handler.
-        executeFieldHandler(component, (req.handlerName + 'Action'), req, res, done);
-      }, function(err) {
-        if (err) {
-          return done(err);
-        }
-
-        router.formio.actions.execute('before', method.name, req, res, function(err) {
-          if (err) {
-            return done(err);
-          }
-
-          // Fix issues with /PUT adding data to the payload with undefined value.
-          if (req.body && req.body.hasOwnProperty('data') && typeof req.body.data === 'undefined') {
-            req.body = _.omit(req.body, 'data');
-          }
-
-          async.eachSeries(flattenedComponents, function(component, done) {
-            executeFieldHandler(component, req.handlerName, req, res, done);
-          }, done);
-        });
-      });
+        executeFieldHandler(component, (req.handlerName + 'Action'), req, res, then);
+      }, done);
     };
 
+    /**
+     * Execute the actions.
+     *
+     * @param req
+     * @param res
+     * @param done
+     */
+    var executeActions = function(handler) {
+      return function(req, res, done) {
+        // If the body is undefined, then omit the body.
+        if (
+          (handler === 'before') &&
+          (req.body && req.body.hasOwnProperty('data') && typeof req.body.data === 'undefined')
+        ) {
+          req.body = _.omit(req.body, 'data');
+        }
+
+        router.formio.actions.execute(handler, method.name, req, res, done);
+      };
+    };
+
+    /**
+     * Execute the field handlers.
+     *
+     * @param req
+     * @param res
+     * @param done
+     */
+    var executeFieldHandlers = function(req, res, done) {
+      async.eachSeries(req.flattenedComponents, function(component, done) {
+        executeFieldHandler(component, req.handlerName, req, res, done);
+      }, done);
+    };
+
+    /**
+     * Ensure that a response is always sent.
+     *
+     * @param req
+     * @param res
+     * @param done
+     */
+    var ensureResponse = function(req, res, done) {
+      if (!res.resource && !res.headersSent) {
+        res.status(200).json(true);
+      }
+      done();
+    };
+
+    // Add before handlers.
     var before = 'before' + method.method;
     handlers[before] = function(req, res, next) {
       req.handlerName = before;
       async.series([
         async.apply(loadCurrentForm, req),
         async.apply(initializeSubmission, req),
+        async.apply(initializeActions, req, res),
         async.apply(validateSubmission, req, res),
-        async.apply(executeHandlers, req, res)
+        async.apply(executeFieldActionHandlers, req, res),
+        async.apply(executeActions('before'), req, res),
+        async.apply(executeFieldHandlers, req, res)
       ], next);
     };
 
+    // Add after handlers.
     var after = 'after' + method.method;
     handlers[after] = function(req, res, next) {
       req.handlerName = after;
-
-      // Execute the router action.
-      router.formio.actions.execute('after', method.name, req, res, function(err) {
-        if (err) {
-          debug.after(err);
-          return next(err);
-        }
-
-        async.eachSeries(util.flattenComponents(req.currentForm.components), function(component, done) {
-          executeFieldHandler(component, req.handlerName, req, res, done);
-        }, function(err) {
-          if (err) {
-            return next(err);
-          }
-
-          // Make sure to send something if no actions sent anything...
-          if (!res.resource && !res.headersSent) {
-            res.status(200).json(true);
-          }
-          next();
-        });
-      });
+      async.series([
+        async.apply(executeActions('after'), req, res),
+        async.apply(executeFieldHandlers, req, res),
+        async.apply(ensureResponse, req, res)
+      ], next);
     };
   });
 

apps/formio/src/models/Action.js
@@ -15,7 +15,8 @@ module.exports = function(formio) {
    *
    * @constructor
    */
-  var Action = function(data, req, res) {
+  var Action = function(data) {
+    this.name = data.name;
     this.title = data.title;
     this.action = data.action;
     this.handler = data.handler;
@@ -23,6 +24,7 @@ module.exports = function(formio) {
     this.priority = data.priority;
     this.form = data.form;
     this.settings = data.settings;
+    this.condition = data.condition;
   };
 
   /**

apps/formio/src/util/util.js
@@ -71,6 +71,9 @@ module.exports = {
     childReq.modelQuery = null;
     childReq.countQuery = null;
 
+    // Delete the actions cache.
+    delete childReq.actions;
+
     // Delete default resourceData from actions
     // otherwise you get an endless loop
     delete childReq.resourceData;

commit 0cc16956683e8aad8c126a4b82bc75dab4afde36

Fix notEqual conditional actions. Was broken in commit 20e932ade

apps/formio/src/actions/actions.js
@@ -196,7 +196,7 @@ module.exports = function(router) {
             if (
               action.condition &&
               action.condition.field &&
-              (action.condition.eq === 'equals') &&
+              (action.condition.eq === 'equals') ===
               (req.body.data[action.condition.field] !== action.condition.value)
             ) {
               execute = false;

apps/formio/test/actions.js
@@ -2188,6 +2188,96 @@ module.exports = function(app, template, hook) {
             done();
           });
       });
+
+      it('Executes a does not equal action when not equal', function(done) {
+        var owner = (app.hasProjects || docker) ? template.formio.owner : template.users.admin;
+        helper = new Helper(owner);
+        helper
+          .project()
+          .resource([
+            {
+              type: 'email',
+              persistent: true,
+              unique: false,
+              protected: false,
+              defaultValue: '',
+              suffix: '',
+              prefix: '',
+              placeholder: 'Enter your email address',
+              key: 'email',
+              label: 'Email',
+              inputType: 'email',
+              tableView: true,
+              input: true
+            },
+            {
+              type: 'selectboxes',
+              label: 'Roles',
+              key: 'roles',
+              input: true,
+              values: [
+                {
+                  label: 'Administrator',
+                  value: 'administrator'
+                },
+                {
+                  label: 'Authenticated',
+                  value: 'authenticated'
+                }
+              ]
+            }
+          ])
+          .action({
+            title: 'Role Assignment',
+            name: 'role',
+            priority: 1,
+            handler: ['after'],
+            method: ['create'],
+            condition: {
+              field: 'email',
+              equal: 'notEqual',
+              value: 'none@example.com'
+            },
+            settings: {
+              association: 'new',
+              type: 'add',
+              role: 'authenticated'
+            }
+          })
+          .submission({
+            email: 'test@example.com',
+            roles: ['test']
+          })
+          .execute(function(err) {
+            if (err) {
+              return done(err);
+            }
+
+            var submission = helper.getLastSubmission();
+            assert(submission.roles.indexOf(helper.template.roles.administrator._id) === -1);
+            assert(submission.roles.indexOf(helper.template.roles.authenticated._id) !== -1);
+            done();
+          });
+      });
+
+      it('Does not execute a does not equal action when equal', function(done) {
+        var owner = (app.hasProjects || docker) ? template.formio.owner : template.users.admin;
+        helper.submission({
+            email: 'none@example.com',
+            roles: ['test']
+          })
+          .execute(function(err) {
+            if (err) {
+              return done(err);
+            }
+
+            var submission = helper.getLastSubmission();
+            assert(submission.roles.indexOf(helper.template.roles.administrator._id) === -1);
+            assert(submission.roles.indexOf(helper.template.roles.authenticated._id) === -1);
+            done();
+          });
+      });
+
     });
   });
 };