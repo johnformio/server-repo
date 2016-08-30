c16685fb6c82270613c5922f2144cba26be47f90
Zack Urben <zackurben@gmail.com>
2016-08-30 16:20:09 -0500

    Adding broken sqlconnector action tests.

--- apps/formio-server/test/mocha.js
+++ apps/formio-server/test/mocha.js
@@ -1301,6 +1301,7 @@ describe('Tests', function() {
         require('./s3')(app, template, hook);
         require('./dropbox')(app, template, hook);
         require('./report')(app, template, hook);
+        require('./actions')(app, template, hook);
       });
     });
   });
