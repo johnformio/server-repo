44cf7efad011fc504846e58edcb5cc622f165e28
Zack Urben <zackurben@gmail.com>
2016-11-04 16:28:06 -0500

    Fixing issue where some projects do not have plans.

---
+++ apps/formio-server/src/db/updates/3.0.7-rc.1.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var debug = {
+  updateProjects: require('debug')('formio:update:3.0.7-rc.1-updateProjects')
+};
+
+/**
+ * Update 3.0.7-rc.1 (to be taken before 3.0.7)
+ *
+ * This update does the following.
+ *
+ *   1.) Finds all formio projects which dont have a valid project plan
+ *   2.) Set invalid project plans to basic
+ *
+ * @param db
+ * @param config
+ * @param tools
+ * @param done
+ */
+module.exports = function(db, config, tools, done) {
+  var projectCollection = db.collection('projects');
+  var valid = ['basic', 'independent', 'team', 'commercial'];
+
+  // fix broken projects
+  projectCollection
+  .update(
+    {
+      deleted: {$eq: null},
+      plan: {$nin: valid}
+    },
+    {
+      $set: {
+        plan: 'basic'
+      }
+    },
+    {
+      upsert: false,
+      multi: true
+    },
+    function(err, results) {
+      if (err) {
+        debug.updateProjects(err);
+        return done(err);
+      }
+
+      debug.updateProjects(results);
+      return done();
+    }
+  );
+};
