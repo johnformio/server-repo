d2005f48afa214534c972e8bfd3e89d8078de362
Travis Tidwell <travis@form.io>
2016-11-19 15:31:19 -0600

    Convert node-uuid to uuid module.

--- apps/formio-server/src/oauth/office365.js
+++ apps/formio-server/src/oauth/office365.js
@@ -2,7 +2,7 @@
 
 var Q = require('q');
 var _ = require('lodash');
-var uuid = require('node-uuid');
+var uuid = require('uuid');
 var AuthenticationContext = require('adal-node').AuthenticationContext;
 
 var util = require('formio/src/util/util');
