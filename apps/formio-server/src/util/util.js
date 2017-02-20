ae4f4adc2b46831018dc563af6b4d61e042f15d5
Travis Tidwell <travis@form.io>
2017-02-20 15:14:05 -0600

    Fixed padding issues in the token handler.

--- apps/formio-server/src/util/util.js
+++ apps/formio-server/src/util/util.js
@@ -1,12 +1,13 @@
+var _ = require('lodash');
 module.exports = {
   tokenRegex: new RegExp(/\[\[\s*token\(\s*([^\)]+\s*)\)\s*,?\s*([0-9]*)\s*\]\]/i),
   ssoToken: function(text) {
     var matches = text.match(this.tokenRegex);
     if (matches && matches.length > 1) {
       var parts = matches[1].split('=');
-      var field = parts[0];
-      var resources = parts[1].split(',');
-      var expireTime = parseInt(matches[2], 10);
+      var field = _.trim(parts[0]);
+      var resources = _.map(parts[1].split(','), _.trim);
+      var expireTime = parseInt(_.trim(matches[2]), 10);
       if (!expireTime || isNaN(expireTime)) {
         expireTime = 120;
       }
