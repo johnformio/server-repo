52abbfb73438d03e7ac651226bf54d68a2f47772
Randall Knutson <randall@form.io>
2017-03-07 10:14:44 -0600

    More tweaks.

--- apps/formio-server/test/s3.js
+++ apps/formio-server/test/s3.js
@@ -190,7 +190,8 @@ module.exports = function(app, template, hook) {
             return done(err);
           }
           assert.equal(res.body.bucket, template.project.settings.storage.s3.bucket);
-          assert.equal(res.body.url, template.project.settings.storage.s3.bucketUrl);
+          // Ignore Expires timestamp as it can be off by a second.
+          assert.equal(res.body.url.replace(/Expires=[0-9]*/, ''), template.project.settings.storage.s3.bucketUrl.replace(/Expires=[0-9]*/, ''));
           assert.equal(res.body.data.key, template.project.settings.storage.s3.startsWith);
           assert.equal(res.body.data.AWSAccessKeyId, template.project.settings.storage.s3.AWSAccessKeyId);
           assert.equal(res.body.data.acl, template.project.settings.storage.s3.acl);
