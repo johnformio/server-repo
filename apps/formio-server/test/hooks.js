ea5a3e265616e298cc1c38a7f82b79d9baeff143
Zack Urben <zackurben@gmail.com>
2017-04-05 16:28:23 -0500

    Fixing form and role template import tests, investigating broken template actions test.

--- apps/formio-server/test/hooks.js
+++ apps/formio-server/test/hooks.js
@@ -26,7 +26,7 @@ module.exports = {
     },
 
     templateRoles: function(map) {
-      let temp = [];
+      let temp = {};
 
       Object.keys(map).forEach(role => {
         assert(map[role].hasOwnProperty('project'));
@@ -38,6 +38,83 @@ module.exports = {
         temp[machineName] = _.omit(map[role], ['project']);
       });
 
+      return temp;
+    },
+
+    templateFormsAndResources: function(map) {
+      let temp = {};
+
+      Object.keys(map).forEach(form => {
+        assert(map[form].hasOwnProperty('project'));
+
+        let machineName = form.split(':');
+        machineName = machineName.pop();
+
+        let entity = _.omit(map[form], ['project']);
+        entity.access = entity.access.map(access => {
+          access.roles =  access.roles.map(roleName => {
+            return (roleName.split(':')).pop();
+          });
+
+          return access;
+        });
+
+        entity.submissionAccess = entity.submissionAccess.map(access => {
+          access.roles = access.roles.map(roleName => {
+            return (roleName.split(':')).pop();
+          });
+
+          return access;
+        });
+
+        // Remove the project from the role payload, for comparison in the open source tests.
+        temp[machineName] = entity;
+      });
+
+      return temp;
+    },
+
+    templateActions: function(map) {
+      let temp = {};
+
+      Object.keys(map).forEach(action => {
+        assert(map[action].hasOwnProperty('form'));
+
+        let machineName = action.split(':');
+        machineName = machineName.pop();
+
+        let entity = map[action];
+        let form = entity.form.split(':');
+        form = form.pop();
+        entity.form = form;
+
+        if (entity.settings && entity.settings.resources) {
+          entity.settings.resources = entity.settings.resources.map(resource => {
+            return (resource.split(':')).pop();
+          });
+        }
+
+        if (entity.settings && entity.settings.resource) {
+          entity.settings.resource = (entity.settings.resource.split(':')).pop();
+        }
+
+        if (entity.settings && entity.settings.role) {
+          if (!(entity.settings.role instanceof Array)) {
+            entity.settings.role =(entity.settings.role.split(':')).pop();
+            return;
+          }
+
+          console.log(entity.settings.role)
+          entity.settings.role = entity.settings.role.map(resource => {
+            return (resource.split(':')).pop();
+          });
+        }
+
+        temp[machineName] = entity;
+      });
+
+      console.log(`temp: ${Object.keys(temp).length}`)
+      console.log(Object.keys(temp))
       return temp;
     }
   }
