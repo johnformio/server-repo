61cdd05bc8408145da531ab925d0f7e8623f579f
Travis Tidwell <travis@form.io>
2017-04-06 22:13:44 -0500

    Fixed the parameters to use the file token.

--- apps/formio-server/src/middleware/download.js
+++ apps/formio-server/src/middleware/download.js
@@ -1,24 +1,31 @@
 var request = require('request');
-var FORMIO_FILES_SERVER = process.env.FORMIO_FILES_SERVER;
+var FORMIO_FILES_SERVER = process.env.FORMIO_FILES_SERVER || 'https://files.form.io';
 module.exports = function(formio) {
+  var cache = require('../cache/cache')(formio);
   return function(req, res, next) {
-    formio.cache.loadCurrentForm(req, function(err, form) {
+    cache.loadCurrentProject(req, function(err, project) {
       if (err) {
         return next(err);
       }
-      formio.cache.loadCurrentSubmission(req, function(err, submission) {
+      let settings = project.settings;
+      formio.cache.loadCurrentForm(req, function(err, form) {
         if (err) {
           return next(err);
         }
-        request({
-          method: 'GET',
-          url: FORMIO_FILES_SERVER + '/pdf/' + req.params.projectId + '/file/' + req.params.fileId + '.pdf',
-          headers: {
-            'x-form': JSON.stringify(form),
-            'x-submission': JSON.stringify(submission),
-            'x-jwt-token': req.headers['x-jwt-token']
+        formio.cache.loadCurrentSubmission(req, function(err, submission) {
+          if (err) {
+            return next(err);
           }
-        }).pipe(res);
+          request({
+            method: 'GET',
+            url: FORMIO_FILES_SERVER + '/pdf/' + req.params.projectId + '/file/' + req.params.fileId + '/download',
+            headers: {
+              'x-form': JSON.stringify(form),
+              'x-submission': JSON.stringify(submission),
+              'x-file-token': settings.filetoken
+            }
+          }).pipe(res);
+        });
       });
     });
   };
