64265adcfd8bbf0aa75645004ea3f6f19507d294
Zack Urben <zackurben@gmail.com>
2017-04-12 15:16:22 -0500

    Fixing external token tests.

--- apps/formio-server/test/externalTokens.js
+++ apps/formio-server/test/externalTokens.js
@@ -3,10 +3,8 @@
 
 let request = require('supertest');
 let assert = require('assert');
-let _ = require('lodash');
-let async = require('async');
 let jwt = require('jsonwebtoken');
-
+let config = require('../config');
 
 module.exports = function(app, template, hook) {
   let Helper = require('formio/test/helper')(app);
@@ -20,7 +18,7 @@ module.exports = function(app, template, hook) {
     before(function(done) {
       return new Promise((resolve, reject) => {
         // Get the old token payload
-        jwt.verify(template.users.user1.token, app.formio.formio.config.jwt.secret, (err, decoded) => {
+        jwt.verify(template.users.user1.token, config.formio.jwt.secret, (err, decoded) => {
           if (err) {
             return reject(err);
           }
@@ -34,6 +32,9 @@ module.exports = function(app, template, hook) {
         payload.user.roles = [
           '000000000000000000000000'
         ];
+
+        // Delete iat to forge generate a new token.
+        delete payload.iat;
 
         // Generate the new custom token.
         customToken = app.formio.formio.auth.getToken(payload);
@@ -65,27 +66,40 @@ module.exports = function(app, template, hook) {
             inputType: 'text',
             input: true
           }
-        ], {
-          access: [
-            {type: 'read_all', roles: ['000000000000000000000000']}
-          ],
-          submissionAccess: [
-            {type: 'read_all', roles: ['000000000000000000000000']}
-          ]
-        })
+        ])
         .execute((err, results) => {
           if (err) {
             return done;
           }
           
           tempForm = results.getForm('externalToken');
-          done();
+          request(app)
+            .put(hook.alter(`url`, `/form/${tempForm._id}`, template))
+            .set(`x-jwt-token`, template.formio.owner.token)
+            .send({
+              access: [
+                {type: 'read_all', roles: ['000000000000000000000000']}
+              ],
+              submissionAccess: [
+                {type: 'read_all', roles: ['000000000000000000000000']}
+              ]
+            })
+            .expect(200)
+            .end((err, res) => {
+              if (err) {
+                return done(err);
+              }
+
+              let response = res.body;
+              tempForm = response;
+              return done();
+            });
         });
     });
 
     it('Create a temporary submission for external token tests', function(done) {
       helper
-        .submission(tempForm, {
+        .submission(`externalToken`, {
           foo: 'bar'
         })
         .execute((err, response) => {
@@ -100,7 +114,7 @@ module.exports = function(app, template, hook) {
 
     it('An anonymous user cannot access the form without access', function(done) {
       request(app)
-        .get(hook.alter(`url`, `/form/${tempForm._id}`))
+        .get(hook.alter(`url`, `/form/${tempForm._id}`, template))
         .expect(401)
         .end(err => {
           if (err) {
@@ -113,7 +127,7 @@ module.exports = function(app, template, hook) {
 
     it('A user with a custom token can access the form with access', function(done) {
       request(app)
-        .get(hook.alter(`url`, `/form/${tempForm._id}`))
+        .get(hook.alter(`url`, `/form/${tempForm._id}`, template))
         .set(`x-jwt-token`, customToken)
         .expect(200)
         .end((err, res) => {
@@ -122,7 +136,7 @@ module.exports = function(app, template, hook) {
           }
 
           let response = res.body;
-          assert.equal(response.name, `customToken`);
+          assert.equal(response.name, `externalToken`);
           assert(response.components);
           assert.equal(response.components.length, 1);
           assert.equal(response.access.length, 1);
@@ -135,7 +149,7 @@ module.exports = function(app, template, hook) {
 
     it('An anonymous user cannot access the submission without access', function(done) {
       request(app)
-        .get(hook.alter(`url`, `/form/${tempForm._id}/submission/${tempSubmission._id}`))
+        .get(hook.alter(`url`, `/form/${tempForm._id}/submission/${tempSubmission._id}`, template))
         .expect(401)
         .end(err => {
           if (err) {
@@ -148,7 +162,7 @@ module.exports = function(app, template, hook) {
 
     it('A user with a custom token can access the submission with access', function(done) {
       request(app)
-        .get(hook.alter(`url`, `/form/${tempForm._id}/submission/${tempSubmission._id}`))
+        .get(hook.alter(`url`, `/form/${tempForm._id}/submission/${tempSubmission._id}`, template))
         .set(`x-jwt-token`, customToken)
         .expect(200)
         .end((err, res) => {
