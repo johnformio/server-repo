d92be647a30b9156957b67fb1df5c66c78ab993b
Travis Tidwell <travis@form.io>
2017-04-26 13:15:44 -0500

    Fix mongoose depcrecation warning for validation.

--- apps/formio-server/src/models/Project.js
+++ apps/formio-server/src/models/Project.js
@@ -2,6 +2,7 @@
 
 var _ = require('lodash');
 var EncryptedProperty = require('../plugins/EncryptedProperty');
+var invalidRegex = /[^0-9a-zA-Z\-]|^\-|\-$/;
 
 module.exports = function(router) {
   var formio = router.formio;
@@ -19,7 +20,44 @@ module.exports = function(router) {
         description: 'The name of the project.',
         required: true,
         maxlength: 63,
-        index: true
+        index: true,
+        validate: [
+          {
+            message: 'A Project domain name may only contain letters, numbers, and hyphens (but cannot start or end with a hyphen)',
+            validator: function(value) {
+              return !invalidRegex.test(value);
+            }
+          },
+          {
+            message: 'This domain is reserved. Please use a different domain.',
+            validator: function(value) {
+              return !formio.config.reservedSubdomains || !_.includes(formio.config.reservedSubdomains, value);
+            }
+          },
+          {
+            isAsync: true,
+            message: 'The Project name must be unique.',
+            validator: function(value, done) {
+              var search = {
+                name: value,
+                deleted: {$eq: null}
+              };
+
+              // Ignore the id if this is an update.
+              if (this._id) {
+                search._id = {$ne: this._id};
+              }
+
+              formio.mongoose.model('project').findOne(search).exec(function(err, result) {
+                if (err || result) {
+                  return done(false);
+                }
+
+                done(true);
+              });
+            }
+          }
+        ]
       },
       description: {
         type: String,
@@ -59,37 +97,6 @@ module.exports = function(router) {
     secret: formio.config.mongoSecret,
     plainName: 'settings'
   });
-
-  // Validation
-  var invalidRegex = /[^0-9a-zA-Z\-]|^\-|\-$/;
-  model.schema.path('name').validate(function(name) {
-    return !invalidRegex.test(name);
-  }, 'A Project domain name may only contain letters, numbers, and hyphens (but cannot start or end with a hyphen)');
-
-  model.schema.path('name').validate(function(name) {
-    return !formio.config.reservedSubdomains || !_.includes(formio.config.reservedSubdomains, name);
-  }, 'This domain is reserved. Please use a different domain.');
-
-  // Validate the uniqueness of the value given for the name.
-  model.schema.path('name').validate(function(value, done) {
-    var search = {
-      name: value,
-      deleted: {$eq: null}
-    };
-
-    // Ignore the id if this is an update.
-    if (this._id) {
-      search._id = {$ne: this._id};
-    }
-
-    formio.mongoose.model('project').findOne(search).exec(function(err, result) {
-      if (err || result) {
-        return done(false);
-      }
-
-      done(true);
-    });
-  }, 'The Project name must be unique.');
 
   // Add machineName to the schema.
   model.schema.plugin(require('formio/src/plugins/machineName'));
