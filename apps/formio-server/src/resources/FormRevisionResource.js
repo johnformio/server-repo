e603d623a2b4c249b50d2ba266acd7973c47ab43
Randall Knutson <randall@form.io>
2017-11-17 11:07:56 -0600

    Move form revisions to a separate resource.

---
+++ apps/formio-server/src/resources/FormRevisionResource.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const Resource = require('resourcejs');
+
+module.exports = function(router, formioServer) {
+  const formio = formioServer.formio;
+
+  let options = {
+    before: [
+      formio.middleware.filterMongooseExists({field: 'deleted', isNull: true}),
+      formio.middleware.bootstrapEntityOwner(false),
+      formio.middleware.formHandler,
+      formio.middleware.formActionHandler('before'),
+      (req, res, next) => {
+        // Always add the resourceId to all queries.
+        req.query['_rid'] =  req.params['formId'];
+        next();
+      }
+    ],
+    after: [
+      formio.middleware.bootstrapFormAccess,
+      formio.middleware.formLoader,
+      formio.middleware.formActionHandler('after'),
+      formio.middleware.filterResourcejsResponse(['deleted', '__v'])
+    ],
+    hooks: {
+      get: {
+        before: (req, res, search, next) => {
+          search['_rid'] = req.params['formId'];
+          // Add vid if set.
+          if (req.params['vId']) {
+            delete search['_id'];
+            search['_vid'] = !isNaN(req.params['vId']) ? parseInt(req.params['vId']) : req.params['vId'];
+          }
+          next();
+        }
+      }
+    }
+  };
+
+  return Resource(
+    router,
+    '/project/:projectId/form/:formId',
+    'v',
+    formio.mongoose.model('formrevision')
+  )
+    .index(options)
+    .get(options);
+};
