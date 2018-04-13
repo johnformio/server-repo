5115225fa27eb52dbbcde2c71a48548eba32bebc
Travis Tidwell <travis@form.io>
2018-04-13 09:22:23 -0500

    Changed how the unique validators are removed.

--- apps/formio-server/src/models/FormRevision.js
+++ apps/formio-server/src/models/FormRevision.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const mongoose = require('mongoose');
+const _ = require('lodash');
 
 module.exports = (router, models) => {
   const schema = models.form.schema.clone();
@@ -32,17 +33,37 @@ module.exports = (router, models) => {
     }
   });
 
-  // Remove the name unique validator.
-  const nameValidators = schema.obj.name.validate || schema.obj.name.validators;
-  schema.path('name', {
-    validators: nameValidators.slice(0, nameValidators.length - 2)
-  });
+  /**
+   * Removes unique validators.
+   *
+   * @param property
+   */
+  const removeUniqueValidator = function(property) {
+    // Remove the name unique validator.
+    const propModifier = {};
+    if (schema.obj[property].validate) {
+      propModifier.validate = schema.obj[property].validate;
+      _.remove(propModifier.validate, (validator) => {
+        return (validator.message.indexOf('must be unique') !== -1);
+      });
+    }
+    else if (schema.obj[property].validators) {
+      propModifier.validators = schema.obj[property].validators;
+      _.remove(propModifier.validators, (validator) => {
+        return (validator.message.indexOf('must be unique') !== -1);
+      });
+    }
+    else {
+      /* eslint-disable no-console */
+      console.warn('Cannot find validators on Form schema.');
+      /* eslint-enable no-console */
+    }
 
-  // Remove the path unique validator.
-  const pathValidators = schema.obj.path.validate || schema.obj.path.validators;
-  schema.path('path', {
-    validators: pathValidators.slice(0, pathValidators.length - 2)
-  });
+    schema.path(property, propModifier);
+  };
+
+  removeUniqueValidator('name');
+  removeUniqueValidator('path');
 
   schema.remove('machineName');
   // This removes the machinename index which will throw errors.
