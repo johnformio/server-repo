756f73e35121437ba5f9d8bd42ed2c5d657c4a48
Randall Knutson <randall@form.io>
2018-08-16 14:41:43 -0500

    Add lint and build.

--- apps/formio-server/compile.js
+++ apps/formio-server/compile.js
@@ -20,7 +20,7 @@ try {
 }
 catch (e) {
   /* eslint-disable no-console */
-  console.error(e);
+  console.log('Destination build dir exists, skipping mkdir.');
   /* eslint-enable no-console */
 }
 
@@ -47,4 +47,11 @@ webpack({
     filename: 'main.js'
   },
   externals: nodeModules
-}).run();
+}).run((err, stats) => {
+  if (err) {
+    /* eslint-disable no-console */
+    console.err(err);
+    return;
+  }
+  console.log('Finished compiling main.js.');
+});
