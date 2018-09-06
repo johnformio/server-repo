da3f6505e375c4753d7e4b528dc0f0d0c02ae082
Yury Rybak <yury.rybak@softensity.com>
2018-09-06 18:25:36 +0300

    FOR-1603,FOR-1639: Fixed Google Sheets.

--- apps/formio-server/src/actions/googlesheet/googleSheet.js
+++ apps/formio-server/src/actions/googlesheet/googleSheet.js
@@ -6,12 +6,15 @@ const formioUtil = require('formio/src/util/util');
 const debug = require('debug')('formio:action:googlesheet');
 const GoogleSheet = require('formio-services/services/GoogleSheet');
 
-module.exports = function(router) {
-  const {formio} = router;
+module.exports = (router) => {
   const {
-    Action,
-    hook
-  } = formio;
+    formio: {
+      Action,
+      cache,
+      resources,
+      hook
+    }
+  } = router;
 
   /**
    * GoogleSheetAction class.
@@ -40,77 +43,78 @@ module.exports = function(router) {
       /**
        * Verifying settings form data and restricting action form loading if any of the settings field data is missing.
        */
-      util.checkOauthParameters(router, req, function(err) {
-        if (err) {
-          return res.status(400).send(err);
-        }
-
-        formio.cache.loadCurrentForm(req, (err, form) => {
-          if (err) {
-            return res.status(400).send(err);
-          }
-
-          if (!form) {
-            return res.status(400).send('No form found.');
-          }
-
-          // Create the panel for all the fields.
-          const fieldPanel = {
-            type: 'panel',
-            theme: 'info',
-            title: 'Google Sheet Fields',
-            input: false,
-            components: []
-          };
-
-          formioUtil.eachComponent(form.components, function(component) {
-            if (component.action !== 'submit' && component.input) {
-              fieldPanel.components.push({
-                type: 'textfield',
-                input: true,
-                label: `${component.label || component.key} Column`,
-                key: component.key,
-                placeholder: 'Enter a Column Key. Example: C',
-                multiple: false
-              });
+      util.checkOauthParameters(router, req)
+        .then(() => new Promise((resolve, reject) => {
+          cache.loadCurrentForm(req, (err, form) => {
+            if (err) {
+              return reject(err);
             }
-          }, true);
 
-          next(null, [
-            {
-              type: 'textfield',
-              label: 'Sheet ID',
-              key: 'sheetID',
-              placeholder: 'Enter the Sheet ID',
-              input: true,
-              validate: {
-                required: true
+            if (!form) {
+              return reject('No form found.');
+            }
+
+            // Create the panel for all the fields.
+            const fieldPanel = {
+              type: 'panel',
+              theme: 'info',
+              title: 'Google Sheet Fields',
+              input: false,
+              components: []
+            };
+
+            formioUtil.eachComponent(form.components, (component) => {
+              if (component.action !== 'submit' && component.input) {
+                fieldPanel.components.push({
+                  type: 'textfield',
+                  input: true,
+                  label: `${component.label || component.key} Column`,
+                  key: component.key,
+                  placeholder: 'Enter a Column Key. Example: C',
+                  multiple: false
+                });
+              }
+            }, true);
+
+            next(null, [
+              {
+                type: 'textfield',
+                label: 'Sheet ID',
+                key: 'sheetID',
+                placeholder: 'Enter the Sheet ID',
+                tooltip: 'Enter the Sheet ID',
+                input: true,
+                validate: {
+                  required: true
+                },
+                multiple: false
               },
-              multiple: false
-            },
-            {
-              type: 'textfield',
-              label: 'Worksheet Name',
-              key: 'worksheetName',
-              placeholder: 'Enter the Worksheet Name. Example: Sheet1',
-              input: true,
-              validate: {
-                required: true
+              {
+                type: 'textfield',
+                label: 'Worksheet Name',
+                key: 'worksheetName',
+                placeholder: 'Enter the Worksheet Name. Example: Sheet1',
+                tooltip: 'Enter the Worksheet Name. Example: Sheet1',
+                input: true,
+                validate: {
+                  required: true
+                },
+                multiple: false
               },
-              multiple: false
-            },
-            {
-              type: 'textfield',
-              label: 'Start Row',
-              tooltip: 'The first row of the data in your spreadsheet.',
-              key: 'spreadSheetStartRow',
-              defaultValue: '2',
-              input: true
-            },
-            fieldPanel
-          ]);
-        });
-      });
+              {
+                type: 'textfield',
+                label: 'Start Row',
+                placeholder: 'The first row of the data in your spreadsheet.',
+                tooltip: 'The first row of the data in your spreadsheet.',
+                key: 'spreadSheetStartRow',
+                defaultValue: '2',
+                input: true
+              },
+              fieldPanel
+            ]);
+          });
+        }))
+        .catch((err) => res.status(400).send(err));
     }
 
     // The actions core execution logic.
@@ -123,7 +127,7 @@ module.exports = function(router) {
       next(); // eslint-disable-line callback-return
 
       // Load the project settings.
-      formio.hook.settings(req, (err, settings) => {
+      hook.settings(req, (err, settings) => {
         const data = _.get(req, 'body.data');
         if (err) {
           debug(err);
@@ -138,49 +142,32 @@ module.exports = function(router) {
         const spreadSheet = new GoogleSheet({
           service: process.env.GOOGLE_SHEETS_SERVICE || ''
         });
-        if (req.method === 'POST') {
-          spreadSheet.addRow(config, this.settings, data).then((result) => {
-            if (!res.resource) {
-              return debug('No resource given in the response.');
-            }
+        if (['POST', 'PUT'].includes(req.method)) {
+          const item = _.get(res, 'resource.item');
+          const rowId = this.getRowId(item);
 
-            // Update the formio submission with an externalId ref to the sheet.
-            formio.resources.submission.model.update(
-              {_id: res.resource.item._id},
-              {
-                $push: {
-                  externalIds: {
-                    type: this.name,
-                    id: result.rowId
+          return (rowId
+            ? spreadSheet.updateRow(config, this.settings, rowId, item.data)
+            : spreadSheet.addRow(config, this.settings, data)
+              .then((result) => {
+                if (!res.resource) {
+                  return debug('No resource given in the response.');
+                }
+
+                // Update the formio submission with an externalId ref to the sheet.
+                return resources.submission.model.findByIdAndUpdate(
+                  res.resource.item._id,
+                  {
+                    $push: {
+                      externalIds: {
+                        type: this.name,
+                        id: result.rowId
+                      }
+                    }
                   }
-                }
-              },
-              (err) => {
-                if (err) {
-                  return debug(err.message || err);
-                }
-              }
-            );
-          }).catch(err => {
-            return debug(err.message || err);
-          });
-        }
-        else if (req.method === 'PUT') {
-          if (!res.resource || !res.resource.item || !res.resource.item.externalIds) {
-            return;
-          }
-
-          // The row number to update.
-          let rowId = _.find(res.resource.item.externalIds, (item) => (item.type === this.name));
-          rowId = _.get(rowId, 'id');
-          if (!rowId) {
-            return;
-          }
-
-          // Update the row with data.
-          spreadSheet.updateRow(config, this.settings, rowId, res.resource.item.data).catch(err => {
-            return debug(err.message || err);
-          });
+                ).exec();
+              }))
+            .catch((err) => debug(err.message || err));
         }
         else if (req.method === 'DELETE') {
           // Only proceed with deleting a row, if applicable to this request.
@@ -190,17 +177,31 @@ module.exports = function(router) {
           }
 
           // The row number to delete.
-          let rowId = _.find(deleted.externalIds, (item) => (item.type === this.name));
-          rowId = _.get(rowId, 'id');
+          const rowId = _.get(deleted.externalIds.find((item) => item.type === this.name), 'id');
           if (!rowId) {
             return;
           }
 
-          spreadSheet.deleteRow(config, this.settings, rowId).catch(err => {
-            return debug(err.message || err);
-          });
+          spreadSheet.deleteRow(config, this.settings, rowId)
+            .catch(err => debug(err.message || err));
         }
       });
+    }
+
+    getRowId(item) {
+      const externalIds = _.get(item, 'externalIds');
+
+      if (!externalIds) {
+        return null;
+      }
+
+      // The row number to update.
+      const rowId = _.get(externalIds.find((item) => item.type === this.name), 'id');
+      if (!rowId) {
+        return null;
+      }
+
+      return rowId;
     }
   }
 
