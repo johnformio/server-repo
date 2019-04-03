fb66eae581eebf45f554fc09c133e2365f4366b2
Travis Tidwell <travis@form.io>
2019-04-02 21:21:34 -0500

    Fixing the remote team access problems.

--- apps/formio-server/src/middleware/remoteAccess.js
+++ apps/formio-server/src/middleware/remoteAccess.js
@@ -32,7 +32,7 @@ module.exports = app => (req, res, next) => {
           const type = access.type.replace('stage_', 'team_');
           roles.forEach(role => {
             if (
-              req.user.teams.indexOf(role) &&
+              (req.user.teams.indexOf(role) !== -1) &&
               permissions.indexOf(type) > permissions.indexOf(response.permission)
             ) {
               response.permission = type;
