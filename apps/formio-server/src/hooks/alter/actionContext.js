32702167e8008973c6b4a7cb3415ca03b5b55506
Randall Knutson <randall@knutson.io>
2019-04-11 11:19:23 -0400

    Fix param order.

--- apps/formio-server/src/hooks/alter/actionContext.js
+++ apps/formio-server/src/hooks/alter/actionContext.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = (app) => (req, params) => {
+module.exports = (app) => (params, req) => {
   params.config = req.currentProject && req.currentProject.hasOwnProperty('config') ? req.currentProject.config : {};
   return params;
 };
