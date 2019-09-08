61700a4c23bbebfc4983a69931435cba2c3319f8
Travis Tidwell <travis@form.io>
2019-09-08 18:29:16 -0500

    Working to fix tests and renamed variable.

--- apps/formio-server/src/hooks/alter/accessInfo.js
+++ apps/formio-server/src/hooks/alter/accessInfo.js
@@ -6,7 +6,7 @@ const _ = require('lodash');
 module.exports = router => async(accessInfo, next) => {
   // Allow filtering to be disabled with environmental variable, with optional project settings override
   const projectSettings = _.get(accessInfo, 'req.currentProject.settings', {});
-  accessInfo.filterEnabled = _.defaultTo(projectSettings.accessEndpointFiltering, config.accessEndpointFiltering);
+  accessInfo.filterEnabled = _.defaultTo(projectSettings.filterAccess, config.filterAccess);
 
   // If filtering is enabled, do project-level filtering here before passing it back to core server
   if (accessInfo.filterEnabled) {
