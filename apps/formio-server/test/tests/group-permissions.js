abf825749a51dcbb75f406451c76ebd23b5726a3
Alex Webb <alexwebb2@gmail.com>
2019-09-09 11:24:37 -0500

    Update formio-server test to factor in filter env flag

--- apps/formio-server/test/tests/group-permissions.js
+++ apps/formio-server/test/tests/group-permissions.js
@@ -1316,6 +1316,31 @@ module.exports = function(app, template, hook) {
         });
       });
 
+      describe('meta access', function() {
+        it('Anonymous users have appropriate form/role access visibility', function(done) {
+          request(app)
+            .get(hook.alter('url', '/access', template))
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function(err, res) {
+              if (err) {
+                return done(err);
+              }
+
+              if (process.env.FILTER_ACCESS === true || process.env.FILTER_ACCESS === 'true') {
+                assert.equal(Object.keys(res.body.roles).length, 0);
+                assert.equal(res.body.forms.testComponentForm, undefined);
+              }
+              else {
+                assert.equal(Object.keys(res.body.roles).length, 3);
+                assert.notEqual(res.body.forms.testComponentForm, undefined);
+              }
+
+              done();
+            });
+        });
+      });
+
       describe('read access', function() {
         before(function() {
           submissions = [];
