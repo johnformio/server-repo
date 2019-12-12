04d68b1ab254da074778fe7219306bcea76ce26d
Alexander Kiseliov <kiselev20060@gmail.com>
2019-12-12 16:03:54 +0300

    Added correct handling of tenant project plan.

---
+++ apps/formio-server/src/middleware/checkTenantProjectPlan.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = function(formio) {
+  return function(req, res, next) {
+    if (req.body.type !== 'tenant') {
+      return next();
+    }
+
+    if (!('project' in req.body)) {
+      return next('Parent project not found.');
+    }
+
+    formio.mongoose.model('project').findOne({
+      _id: req.body.project,
+      plan: 'commercial'
+    }).then((project) => {
+      if (project) {
+        return next();
+      }
+
+      next('Tenants can only be created on Commercial project plan.');
+    });
+  };
+};
