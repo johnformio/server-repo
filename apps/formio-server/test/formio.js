e05a54a73658c042eb412d4e05383d456276ddf4
Yury Rybak <yuryrybak97@gmail.com>
2020-02-25 17:24:37 +0300

    Updated tests.

--- apps/formio-server/test/formio.js
+++ apps/formio-server/test/formio.js
@@ -384,6 +384,7 @@ describe('Initial Tests', function() {
         require('formio/test/actions')(app, template, hook);
         require('formio/test/submission')(app, template, hook);
         require('formio/test/submission-access')(app, template, hook);
+        require('./tests/validate')(app, template, hook);
         require('./tests/analytics')(app, template, hook);
         require('./tests/teams')(app, template, hook);
         require('./tests/env')(app, template, hook);
