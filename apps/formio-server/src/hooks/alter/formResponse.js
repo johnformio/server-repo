69dd3b239110e8dd9d7c00b44a506c91a12d7c98
Travis Tidwell <travis@form.io>
2020-03-14 14:08:50 -0500

    Adding config and modules to revisions. Also allow alias for validate endpoint. Upgrade dependencies.

--- apps/formio-server/src/hooks/alter/formResponse.js
+++ apps/formio-server/src/hooks/alter/formResponse.js
@@ -1,25 +1,36 @@
 'use strict';
 const _ = require('lodash');
 module.exports = app => {
-  const loadFormAlter = {
+  const formResponseAlter = {
     alter: (project, form) => {
+      // Add form configs from public configs.
       if (_.get(project, 'settings.addConfigToForms', false)) {
         form.config = project.config;
       }
+
+      // Add form modules.
       const formModule = _.get(project, 'settings.formModule');
       if (formModule) {
         form.module = formModule;
       }
+
+      // Add recaptcha site keys.
+      if (_.get(form, 'settings.recaptcha.isEnabled')) {
+        _.set(form, 'settings.recaptcha.siteKey', _.get(project, 'settings.recaptcha.siteKey'));
+      }
       return form;
     },
     hook: (form, req, next) => {
+      if (!form || !form.project) {
+        return next(null, form);
+      }
       app.formio.formio.cache.loadProject(req, form.project.toString(), (err, project) => {
         if (err) {
           return next(err);
         }
-        return next(null, loadFormAlter.alter(project, form));
+        return next(null, formResponseAlter.alter(project, form));
       });
     }
   };
-  return loadFormAlter;
+  return formResponseAlter;
 };
