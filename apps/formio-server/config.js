780b9a981af9fc4aa3d7ffc6f0e5de60f2369228
Travis Tidwell <travis@form.io>
2020-10-08 22:04:26 -0500

    Adding vpat and sac to server.

--- apps/formio-server/config.js
+++ apps/formio-server/config.js
@@ -172,6 +172,8 @@ config.hostedPDFServer = getConfig('PDF_SERVER', getConfig('FORMIO_FILES_SERVER'
 config.portalSSO = getConfig('PORTAL_SSO', '');
 config.ssoTeams = Boolean(getConfig('SSO_TEAMS', false) || config.portalSSO);
 config.portalSSOLogout = getConfig('PORTAL_SSO_LOGOUT', '');
+config.vpat = Boolean(getConfig('VPAT', false));
+config.sac = Boolean(getConfig('SAC', false));
 
 // Payeezy fields
 config.payeezy = {
