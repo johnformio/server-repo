b4de6ee8874f5a483ca9f5d65dc3f80afc6bebfd
alexandraRamanenka <60643585+alexandraRamanenka@users.noreply.github.com>
2020-11-05 15:56:20 +0300

    Added configFormio hook

--- apps/formio-server/src/hooks/alter/configFormio.js
+++ apps/formio-server/src/hooks/alter/configFormio.js
@@ -1,7 +1,9 @@
 'use strict';
 const premium = require('@formio/premium/dist/premium-server.min.js');
 
-module.exports = Formio => {
+module.exports = ({Formio}) => {
+  // eslint-disable-next-line no-debugger
+  debugger;
   if (Formio && 'use' in Formio) {
     Formio.use(premium);
   }
