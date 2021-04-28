c106fc40a1081cc3ab61e7679c853669e213ba2a
MaksimFalei <Maksim.Falei@softensity.com>
2021-04-28 15:25:30 +0300

    Fixes an issue when got an error "Cannot read property '_id' of null" and the server was crashing while trying to create a form with revisions using x-admin-key.

--- apps/formio-server/src/hooks/alter/formRoutes.js
+++ apps/formio-server/src/hooks/alter/formRoutes.js
@@ -15,7 +15,11 @@ module.exports = app => routes => {
 
     const body = item.toObject();
     body._rid = body._id;
-    body._vuser = _.get(user, "data.name") || _.get(user, "data.email", user._id);
+
+    if (user) {
+      body._vuser = _.get(user, "data.name") || _.get(user, "data.email", user._id);
+    }
+
     body._vnote = note || '';
     delete body._id;
     delete body.__v;
@@ -52,6 +56,11 @@ module.exports = app => routes => {
     );
   };
 
+  const getRequestUser = (req) => {
+    const user = req.user || (req.adminKey ? {data: {name: 'admin'}} : null);
+    return user;
+  };
+
   routes.hooks.put = {
     before(req, res, item, next) {
       app.formio.formio.util.markModifiedParameters(item, ['components', 'properties']);
@@ -65,7 +74,7 @@ module.exports = app => routes => {
     after(req, res, item, next) {
       app.formio.formio.cache.loadForm(req, null, req.params.formId, (err, form) => {
         if (shouldCreateNewRevision(req, item, form)) {
-          return createVersion(item, req.user, req.body._vnote, next);
+          return createVersion(item, getRequestUser(req), req.body._vnote, next);
         }
         next();
       });
@@ -85,7 +94,7 @@ module.exports = app => routes => {
     after(req, res, item, next) {
       app.formio.formio.cache.loadForm(req, null, req.params.formId, (err, form) => {
         if (shouldCreateNewRevision(req, item, form)) {
-          return createVersion(item, req.user, '', next);
+          return createVersion(item, getRequestUser(req), '', next);
         }
         next();
       });
@@ -98,7 +107,7 @@ module.exports = app => routes => {
         item.revisions &&
         revisionPlans.includes(req.primaryProject.plan)
       ) {
-        return createVersion(item, req.user, req.body._vnote, next);
+        return createVersion(item, getRequestUser(req), req.body._vnote, next);
       }
 
       if (!item) {
