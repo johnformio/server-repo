5d1fb8001d61df3ce54e7bb3b126b1e43f2271b5
Maria-Golomb <mariagolomb.mg@gmail.com>
2021-05-20 18:17:31 +0300

    FIO-2914: added custom componentns to Formio instance

--- apps/formio-server/src/hooks/alter/configFormio.js
+++ apps/formio-server/src/hooks/alter/configFormio.js
@@ -1,5 +1,9 @@
 'use strict';
 const {premium} = require('@formio/premium/dist/premium-server.min.js');
+const fs = require('fs');
+var path = require('path');
+
+const customModulesPath = '../../modules/custom.js';
 
 module.exports = ({Formio}) => {
   // eslint-disable-next-line no-debugger
@@ -7,5 +11,15 @@ module.exports = ({Formio}) => {
     Formio.use(premium);
   }
 
+  fs.access(path.resolve(__dirname, customModulesPath), function(err) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      if (Formio && 'use' in Formio) {
+        Formio.use(require(path.resolve(__dirname, customModulesPath)));
+      }
+    }
+  });
   return Formio;
 };
