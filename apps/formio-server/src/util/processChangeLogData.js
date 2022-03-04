46c53146d58bef2de4a3688a6f858048d6e92b80
Maria-Golomb <76998395+Maria-Golomb@users.noreply.github.com>
2022-03-04 20:55:08 +0100

    FIO-4668: Added ability to render signature and sketchpad components (#986)
    * FIO-4668: Added ability to render signature and sketchpad components
    
    * FIO-4667: added mark of changed submission fields to submission change log
    
    * removed data duplication
    
    * FIO-4673: field labels added to field path

--- apps/formio-server/src/util/processChangeLogData.js
+++ apps/formio-server/src/util/processChangeLogData.js
@@ -1,221 +1,319 @@
 'use strict';
 const _ = require('lodash');
 
-const components = [
-  {
-    "label": "Submission Change Log",
-    "reorder": false,
-    "addAnotherPosition": "bottom",
-    "layoutFixed": false,
-    "enableRowGroups": false,
-    "initEmpty": false,
-    "tableView": false,
-    "defaultValue": [
-        {}
-    ],
-    "key": "changeLogDataGrid",
-    "type": "datagrid",
-    "input": true,
-    "components": [
-        {
-            "label": "Container",
-            "tableView": false,
-            "key": "container",
-            "type": "container",
-            "input": true,
-            "hideLabel": true,
-            "components": [
-                {
-                    "label": "Revision Id",
-                    "labelPosition": "left-left",
-                    "labelWidth": 10,
-                    "labelMargin": 3,
-                    "tableView": true,
-                    "key": "revisionId",
-                    "type": "textfield",
-                    "input": true
-                },
-                {
-                  "label": "Date / Time",
-                  "labelPosition": "left-left",
-                  "labelWidth": 10,
-                  "labelMargin": 3,
-                  "tableView": false,
-                  "enableMinDateInput": false,
-                  "datePicker": {
-                      "disableWeekends": false,
-                      "disableWeekdays": false
-                  },
-                  "enableMaxDateInput": false,
-                  "key": "dateTime",
-                  "type": "datetime",
-                  "input": true,
-                  "widget": {
-                      "type": "calendar",
-                      "displayInTimezone": "viewer",
-                      "locale": "en",
-                      "useLocaleSettings": false,
-                      "allowInput": true,
-                      "mode": "single",
-                      "enableTime": true,
-                      "noCalendar": false,
-                      "format": "yyyy-MM-dd hh:mm a",
-                      "hourIncrement": 1,
-                      "minuteIncrement": 1,
-                      "time_24hr": false,
-                      "minDate": null,
-                      "disableWeekends": false,
-                      "disableWeekdays": false,
-                      "maxDate": null
-                  }
-                },
-                {
-                  "label": "User Id",
-                  "labelPosition": "left-left",
-                  "labelWidth": 10,
-                  "labelMargin": 3,
-                  "tableView": true,
-                  "key": "userId",
-                  "type": "textfield",
-                  "input": true
-                },
-                {
-                  "label": "Reason",
-                  "labelPosition": "left-left",
-                  "labelWidth": 10,
-                  "labelMargin": 3,
-                  "tableView": true,
-                  "key": "reason",
-                  "type": "textarea",
-                  "input": true
-                },
-                {
-                    "label": "Revision Id: ",
-                    "reorder": false,
-                    "addAnotherPosition": "bottom",
-                    "layoutFixed": false,
-                    "enableRowGroups": false,
-                    "initEmpty": false,
-                    "hideLabel": true,
-                    "tableView": false,
-                    "key": "dataGrid1",
-                    "type": "datagrid",
-                    "input": true,
-                    "components": [
-                        {
-                            "label": "Field Path",
-                            "tableView": true,
-                            "key": "fieldPath",
-                            "type": "textarea",
-                            "input": true
-                        },
-                        {
-                          "label": "Previous Value",
-                          "tableView": true,
-                          "key": "previousValue",
-                          "type": "textarea",
-                          "input": true
-                      },
-                        {
-                            "label": "Entry Value",
-                            "tableView": true,
-                            "key": "entityValue",
-                            "type": "textarea",
-                            "input": true
-                        }
-                    ]
-                }
-            ]
-        }
-    ]
-  }
-];
+const setRowCount = (rowNumber) => {
+const cteateFormRow = (form, fieldPath, tableTemplate, components, index, revisionIndex, rowNumber) => {
+    const pathArr = fieldPath.split('/');
+    const key = pathArr.length>1 ? pathArr[pathArr.length - 1] : fieldPath;
 
-const createRow = (entityValue, previousValue, fieldPath) => {
-  entityValue = entityValue? entityValue : ' ';
-  previousValue = previousValue? previousValue : ' ';
-  return ({
-  entityValue: entityValue.constructor && entityValue.constructor.name !== 'Date' ? entityValue.toString() : entityValue.toUTCString(),
-  previousValue: previousValue.constructor && previousValue.constructor.name !== 'Date' ? previousValue.toString() : previousValue.toUTCString(),
-  fieldPath
+    // eslint-disable-next-line no-undef
+    FormioUtils.findComponent(form.components, key, null, (component) => {
+      let valueComponent;
+      if (component) {
+        if (revisionIndex === 0 && !component.label.endsWith('&Delta;')) {
+          component.label = `${component.label} &Delta;`;
+        }
+        if (component.type !== 'signature' && component.type !== 'sketchpad') {
+          valueComponent = {
+            "autoExpand": false,
+            "tableView": true,
+            "key": `fieldPath${rowNumber}`,
+            "type": "textarea",
+            "input": true
+          };
+        }
+        else {
+          valueComponent = component;
+        }
+
+      const rowTemplate = [
+          {
+              "components": [
+                  {
+                      "label": "Field Path",
+                      "autoExpand": false,
+                      "tableView": true,
+                      "key": `fieldPath${rowNumber}`,
+                      "type": "textarea",
+                      "input": true,
+                  }
+              ]
+          },
+          {
+              "components": [
+                {
+                  ...valueComponent,
+                  "key": `previousValue${rowNumber}`,
+                  "label": "Previous Value",
+                }
+              ]
+          },
+          {
+              "components": [
+                {
+                  ...valueComponent,
+                  "key": `entityValue${rowNumber}`,
+                  "label": "Entity Value",
+                }
+              ]
+          }
+      ];
+
+      tableTemplate.rows.push(rowTemplate);
+      tableTemplate.numRows = tableTemplate.rows.length;
+
+      if (index === 0 && rowNumber===revisionIndex) {
+        const outerTemplate =[
+          {
+            "components":
+            [{
+               "label": "Revision Id",
+               "labelPosition": "left-left",
+               "labelWidth": 10,
+               "labelMargin": 3,
+               "tableView": true,
+               "key": `revisionId${rowNumber}`,
+               "type": "textfield",
+               "input": true
+           },
+           {
+             "label": "Date / Time",
+             "labelPosition": "left-left",
+             "labelWidth": 10,
+             "labelMargin": 3,
+             "tableView": false,
+             "enableMinDateInput": false,
+             "datePicker": {
+                 "disableWeekends": false,
+                 "disableWeekdays": false
+             },
+             "enableMaxDateInput": false,
+             "key": `dateTime${rowNumber}`,
+             "type": "datetime",
+             "input": true,
+             "widget": {
+                 "type": "calendar",
+                 "displayInTimezone": "viewer",
+                 "locale": "en",
+                 "useLocaleSettings": false,
+                 "allowInput": true,
+                 "mode": "single",
+                 "enableTime": true,
+                 "noCalendar": false,
+                 "format": "yyyy-MM-dd hh:mm a",
+                 "hourIncrement": 1,
+                 "minuteIncrement": 1,
+                 "time_24hr": false,
+                 "minDate": null,
+                 "disableWeekends": false,
+                 "disableWeekdays": false,
+                 "maxDate": null
+             }
+           },
+           {
+             "label": "User Id",
+             "labelPosition": "left-left",
+             "labelWidth": 10,
+             "labelMargin": 3,
+             "tableView": true,
+             "key": `userId${rowNumber}`,
+             "type": "textfield",
+             "input": true
+           },
+           {
+             "label": "Reason",
+             "labelPosition": "left-left",
+             "labelWidth": 10,
+             "labelMargin": 3,
+             "tableView": true,
+             "key": `reason${rowNumber}`,
+             "type": "textarea",
+             "input": true
+           },
+           tableTemplate
+         ]
+         },
+         ];
+
+         components[0].rows.push(
+          outerTemplate
+        );
+        components[0].numRows = components[0].rows.length;
+      }
+      }
+    });
+};
+
+const updatePath = (path, formComponents) => {
+  const pathArr = path.split('/');
+  const normalizeLabel = (str) => str.endsWith('&Delta;') ? str.slice(0, str.length-7) : str;
+  let prevElement;
+  const pathLabel = pathArr.map(key => {
+    // eslint-disable-next-line no-undef
+    const element = FormioUtils.getComponent(formComponents, key);
+    if (element && element.label) {
+      if (element.components) {
+        prevElement = element;
+      }
+      return normalizeLabel(element.label);
+    }
+    else {
+      if (prevElement) {
+        // eslint-disable-next-line no-undef
+        const innerElement = FormioUtils.getComponent(prevElement.components, key);
+        return normalizeLabel(innerElement && innerElement.label ? innerElement.label : key);
+      }
+    }
+  });
+  return pathLabel.join('/ ');
+};
+
+const createSubmissionRow = (entityValue, previousValue, fieldPath, changes, revision, form) => {
+  entityValue = entityValue===false || entityValue? entityValue : ' ';
+  previousValue = previousValue===false || previousValue? previousValue : ' ';
+
+  Object.assign( changes, {
+  [`revisionId${rowNumber}`]: revision._id,
+  [`dateTime${rowNumber}`]: revision.modified,
+  [`userId${rowNumber}`]: revision._vuser,
+  [`reason${rowNumber}`]: revision.metadata.previousData? revision._vnote: 'Initial submission',
+  [`entityValue${rowNumber}`]: entityValue.constructor && entityValue.constructor.name !== 'Date' ? entityValue.toString() : entityValue.toUTCString(),
+  [`previousValue${rowNumber}`]: previousValue.constructor && previousValue.constructor.name !== 'Date' ? previousValue.toString() : previousValue.toUTCString(),
+  [`fieldPath${rowNumber}`]: updatePath(fieldPath, form.components)
 });
+};
+
+const createRow = (entityValue, previousValue, fieldPath, form, changes, tableTemplate, revision, components, index, revisionIndex) => {
+  cteateFormRow(form, fieldPath, tableTemplate, components, index, revisionIndex, rowNumber);
+  createSubmissionRow(entityValue, previousValue, fieldPath, changes, revision, form);
+  rowNumber = rowNumber + 1;
+};
+
+return createRow;
 };
 
 module.exports = (changelog, form, submission) => {
   if (changelog) {
-    form.components.push(...components);
+    const rowNumber = 0;
+    const createRow = setRowCount(rowNumber);
+    const changeLogForm = [];
 
-    const revisionData = changelog.map(revision => {
-      const changes = [];
-      revision.metadata.jsonPatch.forEach(change => {
+    const revisionData = changelog.map((revision, revisionIndex) => {
+       const components = [
+        {
+          label: 'Outer Table',
+          numRows: 1,
+          cellAlignment: 'left',
+          key: 'changeOuterLogTable',
+          type: 'table',
+          input: false,
+          tableView: false,
+          condensed: true,
+          rows: []
+        }
+      ];
+      const changes = {};
+      const tableTemplate = {
+        label: 'Table',
+        numRows: 0,
+        cellAlignment: 'left',
+        key: 'changeLogTable',
+        type: 'table',
+        input: false,
+        tableView: false,
+        bordered: true,
+        rows: []
+      };
+      revision.metadata.jsonPatch.forEach((change, index) => {
         if (Array.isArray(change.value)) {
           change.value.forEach((val, valIndex) => {
             if (val.value && val.path) {
-              changes.push({
+              Object.assign(changes, {
                 entityValue: val.value,
                 previousValue: _.get(revision.metadata.previousData, `${val.path.slice(6).replace(/\//g, '.')}`),
                 fieldPath: val.path.slice(6)
               });
             }
             else {
-              Object.keys(val).forEach((entry, index) => changes.push(
+              Object.keys(val).forEach((entry, index) =>
                 createRow(
                   val[entry],
                   _.get(revision.metadata.perviousData,`${change.path.slice(6).replace(/\//g, '.')}[${valIndex}]${entry}`),
-                  `${change.path.slice(6)}/${valIndex}/${entry}`)
-                ));
+                  `${change.path.slice(6)}/${valIndex}/${entry}`,
+                  form,
+                  changes,
+                  tableTemplate,
+                  revision,
+                  components,
+                  index,
+                  revisionIndex
+                  )
+                );
             }
           });
         }
         else if (change.value && typeof change.value === 'object' && change.value.constructor.name !== 'Date') {
           _.forIn(change.value, (val, key) => {
-            changes.push(
               createRow(
                 val,
                 revision.data[change.path],
-                `${change.path.slice(6)}/${key}`
-                )
-            );
+                `${change.path.slice(6)}/${key}`,
+                form,
+                changes,
+                tableTemplate,
+                revision,
+                components,
+                index,
+                revisionIndex);
           });
         }
         else {
           const previousValue = _.get(revision.metadata.previousData, change.path.slice(6).replace(/\//g, '.'));
           if (previousValue && typeof previousValue === 'object' && !Array.isArray(previousValue) && previousValue.constructor.name !== 'Date') {
-          _.forIn(previousValue, (val, key) => changes.push(
+          _.forIn(previousValue, (val, key) =>
             createRow(
               change.op === "remove" ? '' : val,
               _.get(revision.metadata.previousData, `${change.path.slice(6).replace(/\//g, '.')}.${key}`),
-              change.path.slice(6)
-              )
-          ));
+              change.path.slice(6),
+              form,
+              changes,
+              tableTemplate,
+              revision,
+              components,
+              index,
+              revisionIndex)
+          );
           }
-else {
-            return changes.push(
-              createRow(
+          else {
+            return createRow(
                 change.op === "remove" ? '' : change.value,
                 previousValue,
-                change.path.slice(6)
-                )
-            );
+                change.path.slice(6),
+                form,
+                changes,
+                tableTemplate,
+                revision,
+                components,
+                index,
+                revisionIndex);
           }
         }
       });
 
-      return ({
-        container: {
-          revisionId: revision._id,
-          dateTime: revision.modified,
-          userId: revision._vuser,
-          reason: revision.metadata.previousData? revision._vnote: 'Initial submission',
-          dataGrid1: changes
-        }
-      });
+      changeLogForm.push(...components);
+      return changes;
     });
 
-    const data = {
-      changeLogDataGrid: revisionData
-    };
+    form.components.push({
+      legend: "Submission Change Log",
+      key: "fieldSet",
+      type: "fieldset",
+      label: "Submission Change Log",
+      input: false,
+      tableView: false,
+      components: changeLogForm
+    });
 
-    Object.assign(submission.data, data);
+    revisionData.forEach(revision => Object.assign(submission.data, revision));
   }
 };
