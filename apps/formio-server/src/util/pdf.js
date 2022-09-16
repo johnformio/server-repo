a44b71d01257cb1c1f4b80ddc14f03f9f4a1e912
alexandraRamanenka <60643585+alexandraRamanenka@users.noreply.github.com>
2022-09-16 16:59:08 +0200

    Get the default PDF server URL based on the app origin (#1090)

--- apps/formio-server/src/util/pdf.js
+++ apps/formio-server/src/util/pdf.js
@@ -8,10 +8,14 @@ module.exports = {
       if (project.settings && project.settings.pdfserver) {
         localServer = project.settings.pdfserver;
       }
+      else if (project.settings && project.settings.appOrigin) {
+        localServer = `${project.settings.appOrigin}/pdf`;
+      }
       else {
         localServer = 'https://files.form.io';
       }
     }
+
     return {
       local: localServer,
       public: (project.settings && project.settings.pdfserver) || localServer
