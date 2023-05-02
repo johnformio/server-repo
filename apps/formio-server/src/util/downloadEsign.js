3d16aaeb371d62dfd687febbbdb23d51932b616a
Alexey Nikipelau <alexey.nikipelau@form.io>
2023-05-02 16:23:22 +0300

    removed redundant arguments

--- apps/formio-server/src/util/downloadEsign.js
+++ apps/formio-server/src/util/downloadEsign.js
@@ -1,8 +1,8 @@
 'use strict';
 const fetch = require('formio/src/util/fetch');
 
-module.exports = (formioServer) => {
-    const BoxSign = require('../actions/esign/integrations/BoxSign')(formioServer);
+module.exports = () => {
+    const BoxSign = require('../actions/esign/integrations/BoxSign')();
     return async (project, submission) => {
         switch (submission.data.esign.provider) {
             case 'Box Sign':
