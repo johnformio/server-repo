f82e96ea749b5469ecb3e29d27a05a230a283135
ICX\Tatsiana.Hashtold <tanya@form.io>
2023-08-23 11:03:22 +0300

    FIO-7229: fixed an issue where stages did not inherit parent project plan in hosted env

--- apps/formio-server/src/middleware/projectDefaultPlan.js
+++ apps/formio-server/src/middleware/projectDefaultPlan.js
@@ -1,21 +1,20 @@
 'use strict';
 const config = require('../../config');
 module.exports = (formioServer) => (req, res, next) => {
+  if (req.method !== 'POST') {
+    return next();
+  }
+  //child project (stage) must inherit parent`s plan
+  if (req.parentProject && req.parentProject.plan && req.body.type === 'stage') {
+    req.body.plan = req.parentProject.plan;
+  }
   // Don't change plans on hosted
   if (config.formio.hosted) {
     return next();
   }
 
-  if (req.method !== 'POST') {
-    return next();
-  }
-
   // If not attempting to set a plan it will default already.
   if (!req.body.hasOwnProperty('plan')) {
-    //child project (stage) must inherit parent`s plan
-    if (req.parentProject && req.parentProject.plan && req.body.type === 'stage') {
-      req.body.plan = req.parentProject.plan;
-    }
     return next();
   }
 
