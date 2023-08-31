082a731fa1e8afcdfd80d474968a40f8358052a4
Roman <romaletsuk@gmail.com>
2023-08-31 19:09:47 +0300

    fixed issue with getting settings for primary admin (#1304)

--- apps/formio-server/src/resources/ProjectResource.js
+++ apps/formio-server/src/resources/ProjectResource.js
@@ -44,7 +44,7 @@ module.exports = (router, formioServer) => {
 
   const projectSettings = (req, res, next) => {
     // Allow admin key
-    if (req.adminKey || (req.apiKey && req.isAdmin)) {
+    if (req.adminKey || req.isAdmin) {
       decryptSettings(res);
       formioServer.formio.audit('PROJECT_SETTINGS', req);
       return next();
