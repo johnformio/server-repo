2ebf852064c5965a861a98c270711c5cd93f6693
Alexey Nikipelau <alexey.nikipelau@form.io>
2024-02-21 17:41:16 +0300

    add evaluateProcessMocked

--- packages/vm/packages/core/src/core/evaluateProcess.ts
+++ packages/vm/packages/core/src/core/evaluateProcess.ts
@@ -1,4 +1,6 @@
+import * as FormioCore from '@formio/core';
 import { evaluate } from './evaluate';
+import { RootShim } from './RootShim';
 
 const code = `
 root = new RootShim(context.form, context.submission);
@@ -53,4 +55,41 @@ export async function evaluateProcess(options: EvaluateProcessorsOptions): Promi
     code,
   });
   return result as EvaluateProcessorsResult;
+}
+
+// Does the same as `evaluateProcess`, but omits call to evaluate
+// So it's possible to debug core functions outside of the vm
+// Should be used only for development
+export async function evaluateProcessMocked(options: EvaluateProcessorsOptions): Promise<EvaluateProcessorsResult> {
+  const submission = JSON.parse(JSON.stringify(options.submission));
+  const context: any = {
+    form: options.form,
+    components: options.form.components,
+    submission: submission,
+    data: submission.data,
+    scope: options.scope || {},
+    config: {
+      server: true,
+      token: options.token || '',
+    }
+  };
+  const root = new RootShim(context.form, context.submission);
+  context.instances = root.instanceMap;
+  const data = context.data;
+  
+  if (context.form.module) {
+    // Wrap with parenthesis to return object, not function
+    const formModule = eval( '(' + context.form.module + ')');
+    const evalContext = formModule?.options?.form?.evalContext;
+  
+    if (evalContext) {
+      const evalContextFn = (context: any) => Object.assign({}, context, evalContext);
+      context.evalContext = evalContextFn;
+    }
+  }
+  
+  context.processors = FormioCore.ProcessTargets.evaluator;
+  const scope = FormioCore.processSync(context);
+  
+  return { scope, data };
 }
