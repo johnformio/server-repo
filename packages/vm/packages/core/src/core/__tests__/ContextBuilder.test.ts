3b0101bd92bc4fad91841b7dab61271e2a57d645
brendanjbond <brendanbond@gmail.com>
2024-03-28 01:23:34 -0500

    add tests

--- packages/vm/packages/core/src/core/__tests__/ContextBuilder.test.ts
+++ packages/vm/packages/core/src/core/__tests__/ContextBuilder.test.ts
@@ -1,40 +1,42 @@
 import { expect } from 'chai';
 import ContextBuilder from '../ContextBuider';
 import { Context } from 'isolated-vm';
-import * as core from '@formio/core'
 
 describe('Test ContextBuilder', () => {
-  let context: Context = null as any;
-  it('should create context with dependecies', async () => {
-    context = await ContextBuilder
-      .fromDefaultIsolate()
-      .withLodash()
-      .withMoment()
-      .withCore()
-      .withNunjucks()
-      .build();
-    
-    expect(context).to.be.an('object');
-    expect(context).to.have.property('evalSync');
-  });
+    let context: Context = null as any;
+    it('should create context with dependecies', async () => {
+        context = await ContextBuilder.fromDefaultIsolate()
+            .withLodash()
+            .withMoment()
+            .withCore()
+            .withNunjucks()
+            .build();
 
-  it('should evaluate lodash', () => {
-    const result = context.evalSync('_.camelCase("hello world")');
-    expect(result).to.equal('helloWorld');
-  });
+        expect(context).to.be.an('object');
+        expect(context).to.have.property('evalSync');
+    });
 
-  it('should evaluate moment', () => {
-    const result = context.evalSync('moment(0).format("YYYY-MM-DD")');
-    expect(result).to.equal('1970-01-01');
-  });
+    it('should evaluate lodash', () => {
+        const result = context.evalSync('_.camelCase("hello world")');
+        expect(result).to.equal('helloWorld');
+    });
 
-  it('should evaluate core', () => {
-    const result = context.evalSync('FormioCore.isEmpty(null)');
-    expect(result).to.equal(true);
-  });
+    it('should evaluate moment', () => {
+        const result = context.evalSync('moment(0).utc().format("YYYY-MM-DD")');
+        expect(result).to.equal('1970-01-01');
+    });
 
-  it('should evaluate code with nunjucks', () => {
-    const result = context.evalSync('nunjucks.renderString("Hello {{ name }}", { name: "World" })');
-    expect(result).to.equal('Hello World');
-  });
+    it('should evaluate core', () => {
+        const result = context.evalSync(
+            'FormioCore.Utils.getComponentKey({ key: "textField", type: "textfield", input: true})',
+        );
+        expect(result).to.equal('textField');
+    });
+
+    it('should evaluate code with nunjucks', () => {
+        const result = context.evalSync(
+            'nunjucks.renderString("Hello {{ name }}", { name: "World" })',
+        );
+        expect(result).to.equal('Hello World');
+    });
 });
