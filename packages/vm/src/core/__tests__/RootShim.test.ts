5748d6400b5a80244b33cfff568f2209db15b80b
brendanjbond <brendanbond@gmail.com>
2024-04-02 06:15:51 -0500

    RootShim uses eachComponent and add tests

--- packages/vm/src/core/__tests__/RootShim.test.ts
+++ packages/vm/src/core/__tests__/RootShim.test.ts
@@ -42,6 +42,28 @@ describe('getComponent', () => {
         expect(component.component.key).to.be.equal('textField');
     });
 
+    it('should return a component at an exact path if it exists and there is no data associated with that component', () => {
+        const components = [
+            {
+                type: 'datagrid',
+                key: 'dataGrid',
+                components: [
+                    {
+                        type: 'textfield',
+                        key: 'textField',
+                        label: 'Text Field',
+                        input: true,
+                    },
+                ],
+            },
+        ];
+        const root = new RootShim({ components }, { data: {} });
+        const component = root.getComponent('dataGrid[0].textField');
+        expect(component).to.be.instanceOf(InstanceShim);
+        expect(component.component.key).to.be.equal('textField');
+        expect(component.component.label).to.be.equal('Text Field');
+    });
+
     it('should return a component (InstanceShim) at a path with the final pathname segment matching the path argument if it exists', () => {
         const components = [
             {
@@ -64,5 +86,27 @@ describe('getComponent', () => {
         const component = root.getComponent('textField');
         expect(component).to.be.instanceOf(InstanceShim);
         expect(component.component.key).to.be.equal('textField');
+    });
+
+    it('should return a component (InstanceShim) at a path with the final pathname segment matching the path argument if it exists and there is no data associated with the component', () => {
+        const components = [
+            {
+                type: 'datagrid',
+                key: 'dataGrid',
+                components: [
+                    {
+                        type: 'textfield',
+                        key: 'textField',
+                        label: 'Text Field',
+                        input: true,
+                    },
+                ],
+            },
+        ];
+        const root = new RootShim({ components }, { data: {} });
+        const component = root.getComponent('textField');
+        expect(component).to.be.instanceOf(InstanceShim);
+        expect(component.component.key).to.be.equal('textField');
+        expect(component.component.label).to.be.equal('Text Field');
     });
 });
