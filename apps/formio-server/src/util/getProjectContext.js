43cd1fd5fb55636482c6604c6f5e5a3956a77c07
mikekotikov <kmikecoder@gmail.com>
2024-04-11 14:18:02 +0300

    FIO-8115: throwing error when type is not provided

--- apps/formio-server/src/util/getProjectContext.js
+++ apps/formio-server/src/util/getProjectContext.js
@@ -2,7 +2,12 @@
 const _ = require('lodash');
 
 module.exports = (req, isNew, res = null, postCreate = false, app = null) => {
-    let type = isNew ? _.get(req, 'body.type', 'project') : _.get(req, 'currentProject.type', req.body.type || 'project');
+    let type =  isNew ? _.get(req, 'body.type') : _.get(req, 'currentProject.type', req.body.type);
+
+   if (!type) {
+    throw new Error("The type should be provided");
+   }
+
     if (postCreate) {
         type = res.resource?.item?.type;
 
