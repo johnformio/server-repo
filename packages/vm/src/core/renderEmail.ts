8ed621c3e3efdb11c5d056fa933610b357fdde18
Alexey Nikipelau <alexey.nikipelau@form.io>
2024-04-19 13:54:55 +0300

    add timeout arg to renderEmail

--- packages/vm/src/core/renderEmail.ts
+++ packages/vm/src/core/renderEmail.ts
@@ -6,74 +6,83 @@ import { evaluate } from '..';
 import macros from './deps/nunjucks-macros';
 
 export type RenderEmailOptions = {
-  render: any,
-  context: any,
-}
+    render: any;
+    context: any;
+    timeout: number;
+};
 
-export async function renderEmail({ render, context = {} }: RenderEmailOptions): Promise<string> {
-  if (context._private) {
-    delete context._private;
-  }
-  context.macros = macros;
-
-  const renderMethod = getRenderMethod(render);
-
-  const data: any = {
-    input: omitUndefined(render),
-    context,
-    submissionTableHtml: null,
-  };
-
-  if (renderMethod === 'dynamic') {
-    try {
-      // @ts-expect-error Premium is included in the server build.
-      const premium = await import('@formio/premium/dist/premium-server.min.js');
-      Formio.use(premium);
+export async function renderEmail({
+    render,
+    context = {},
+    timeout = 500,
+}: RenderEmailOptions): Promise<string> {
+    if (context._private) {
+        delete context._private;
     }
-    // eslint-disable-next-line no-empty
-    catch {}
-    const form = await (new Form(context.form, {
-      server: true,
-      noeval: true,
-      noDefaults: true
-    })).ready;
-  
-    form.setValue({ data: context.data }, { sanitize: true });
+    context.macros = macros;
 
-    // Set visibility of hidden components.
-    // This is necessary to ensure that hidden components are not rendered in the email.
-    if (context?.scope?.clearHidden) {
-      for (const hiddenCompPath in context.scope.clearHidden) {
-        form.getComponent(hiddenCompPath).visible = !context.scope.clearHidden[hiddenCompPath];
-      }
+    const renderMethod = getRenderMethod(render);
+
+    const data: any = {
+        input: omitUndefined(render),
+        context,
+        submissionTableHtml: null,
+    };
+
+    if (renderMethod === 'dynamic') {
+        try {
+            // @ts-expect-error Premium is included in the server build.
+            const premium = await import(
+                '@formio/premium/dist/premium-server.min.js'
+            );
+            Formio.use(premium);
+        } catch {
+            // eslint-disable-next-line no-empty
+        }
+        const form = await new Form(context.form, {
+            server: true,
+            noeval: true,
+            noDefaults: true,
+        }).ready;
+
+        form.setValue({ data: context.data }, { sanitize: true });
+
+        // Set visibility of hidden components.
+        // This is necessary to ensure that hidden components are not rendered in the email.
+        if (context?.scope?.clearHidden) {
+            for (const hiddenCompPath in context.scope.clearHidden) {
+                form.getComponent(hiddenCompPath).visible =
+                    !context.scope.clearHidden[hiddenCompPath];
+            }
+        }
+
+        const submissionTableHtml = form.getView(context.data, {
+            email: true,
+        });
+
+        data.submissionTableHtml = submissionTableHtml;
     }
 
-    const submissionTableHtml = form.getView(context.data, {
-      email: true
+    const res = await evaluate({
+        deps: ['lodash', 'moment', 'core', 'nunjucks'],
+        data: data,
+        code: getScript(render),
+        timeout,
     });
-
-    data.submissionTableHtml = submissionTableHtml;
-  }
-
-  const res = await evaluate({
-    deps: ['lodash', 'moment', 'core', 'nunjucks'],
-    data: data,
-    code: getScript(render),
-  });
-  return res as string;
+    return res as string;
 }
 
 function getScript(data: any) {
-  if (_.isString(data)) {
-    // Script to render a single string.
-    return `
+    if (_.isString(data)) {
+        // Script to render a single string.
+        return `
       environment.params = context;
       output = unescape(environment.renderString(sanitize(input), context));
     `;
-  }
+    }
 
-  // Script to render an object of properties.
-  return `
+    // Script to render an object of properties.
+    return `
     environment.params = context;
     var rendered = {};
     for (let prop in input) {
@@ -87,17 +96,16 @@ function getScript(data: any) {
     }
     output = rendered;
   `;
-};
+}
 
 function getRenderMethod(render: any) {
-  let renderMethod = 'static';
-  if (process.env.RENDER_METHOD) {
-    renderMethod = process.env.RENDER_METHOD;
-  }
-  else if (render && render.renderingMethod) {
-    renderMethod = render.renderingMethod;
-  }
-  return renderMethod;
+    let renderMethod = 'static';
+    if (process.env.RENDER_METHOD) {
+        renderMethod = process.env.RENDER_METHOD;
+    } else if (render && render.renderingMethod) {
+        renderMethod = render.renderingMethod;
+    }
+    return renderMethod;
 }
 
 const omitUndefined = (obj: any) => _.omitBy(obj, _.isUndefined);
