4769d58006bdec2d3414bf4ec405bbe628056fe6
brendanjbond <brendanbond@gmail.com>
2024-07-11 09:19:55 +0100

    update for snyk vulns and logging

--- apps/formio-server/src/middleware/projectOwner.js
+++ apps/formio-server/src/middleware/projectOwner.js
@@ -1,6 +1,7 @@
-'use strict';
+"use strict";
 
-const _ = require('lodash');
+const _ = require("lodash");
+const debug = require("debug")("formio:projectOwner");
 
 module.exports = function(formioServer) {
   const formio = formioServer.formio;
@@ -8,42 +9,44 @@ module.exports = function(formioServer) {
     // Only allow admins to change owner for now.
     if (req.projectId && req.isAdmin && req.adminKey) {
       const projectObj = await formio.cache.updateProject(req.projectId, {
-        owner: formioServer.formio.util.idToBson(req.body.owner)
+        owner: formioServer.formio.util.idToBson(req.body.owner),
       });
 
       try {
         const adminRole = await formio.resources.role.model.findOne({
           project: projectObj._id,
-          title: 'Administrator',
-          deleted: {$eq: null}
+          title: "Administrator",
+          deleted: {$eq: null},
         });
         if (!adminRole) {
-          return res.send(projectObj);
+          return res.json(projectObj);
         }
         // Find the Project owner by id, and add the administrator role of this Project to their roles.
         const owner = await formio.resources.submission.model.findOne({
           _id: projectObj.owner.toString(),
-          deleted: {$eq: null}
+          deleted: {$eq: null},
         });
-          if (!owner) {
-            return res.send(projectObj);
-          }
+        if (!owner) {
+          return res.json(projectObj);
+        }
 
-          // Attempt to remove array with one null element, inserted by mongo.
-          owner.roles = _.filter(owner.roles || []);
+        // Attempt to remove array with one null element, inserted by mongo.
+        owner.roles = _.filter(owner.roles || []);
 
-          // Add the administrative roles of this Project to the creators roles.
-          owner.roles.push(adminRole.toObject()._id);
+        // Add the administrative roles of this Project to the creators roles.
+        owner.roles.push(adminRole.toObject()._id);
 
-          await formio.resources.submission.model.updateOne({
-            _id: owner._id
+        await formio.resources.submission.model.updateOne(
+          {
+            _id: owner._id,
           },
-          {$push: {roles: owner.roles}}
-          );
-          return res.send(projectObj);
+          {$push: {roles: owner.roles}},
+        );
+        return res.json(projectObj);
       }
       catch (err) {
-        return res.send(projectObj);
+        debug(err);
+        return res.json(projectObj);
       }
     }
     else {
