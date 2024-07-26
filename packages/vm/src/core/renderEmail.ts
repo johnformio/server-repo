f3eb131995b6208d32eeda423f5b61f5fc779004
Roman <romaletsuk@gmail.com>
2024-07-26 20:34:00 +0300

    FIO-8626: fixed hidden components rendering in emal (#28)

--- packages/vm/src/core/renderEmail.ts
+++ packages/vm/src/core/renderEmail.ts
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-
+import * as FormioCore from '@formio/core';
 import { Formio, Form } from '@formio/js';
 import { evaluate } from '..';
 
@@ -49,12 +49,18 @@ export async function renderEmail({
 
         // Set visibility of hidden components.
         // This is necessary to ensure that hidden components are not rendered in the email.
-        if (context?.scope?.clearHidden) {
-            for (const hiddenCompPath in context.scope.clearHidden) {
-                form.getComponent(hiddenCompPath).visible =
-                    !context.scope.clearHidden[hiddenCompPath];
-            }
-        }
+        FormioCore.Utils.eachComponent(
+            form.components,
+            (component: any, path: any) => {
+                const conditionalComp = context?.scope?.conditionals?.find(
+                    (condition: any) => condition.path === path,
+                );
+                const hidden = conditionalComp
+                    ? conditionalComp.conditionallyHidden
+                    : context?.componentsWithPath[path]?.hidden;
+                component.visible = !hidden;
+            },
+        );
 
         const submissionTableHtml = form.getView(context.data, {
             email: true,
