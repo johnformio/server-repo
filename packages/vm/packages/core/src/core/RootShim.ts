2e03bd68f98e8292506f30af3877b0978b421101
brendanjbond <brendanbond@gmail.com>
2025-01-10 15:25:47 -0600

    1. updates dependencies 2. updates InstanceShim's instanceMap to account for different pathing options

--- packages/vm/packages/core/src/core/RootShim.ts
+++ packages/vm/packages/core/src/core/RootShim.ts
@@ -22,43 +22,63 @@ export class RootShim {
                 component: FormioCore.Component,
                 data: any,
                 row: any,
-                path: any,
-                componentComponents: any,
-                dataIndex: any,
+                compPath: any,
+                components: any,
+                index: any,
+                parent: any,
+                paths: FormioCore.ComponentPaths,
             ) => {
-                if (!this.instanceMap[path]) {
-                    this.instanceMap[path] = new InstanceShim(
-                        component,
-                        this,
-                        submission.data,
-                        path,
-                        dataIndex,
-                    );
-                    this.components.push(this.instanceMap[path]);
+                const {
+                    path,
+                    fullPath,
+                    fullLocalPath,
+                    dataPath,
+                    localDataPath,
+                } = paths;
+                const instance = new InstanceShim(
+                    component,
+                    this,
+                    submission.data,
+                    dataPath ?? path ?? component.key,
+                    index,
+                );
+                this.components.push(instance);
+                if (path && !this.instanceMap[path]) {
+                    this.instanceMap[path] = instance;
+                }
+                if (fullPath && !this.instanceMap[fullPath]) {
+                    this.instanceMap[fullPath] = instance;
+                }
+                if (fullLocalPath && !this.instanceMap[fullLocalPath]) {
+                    this.instanceMap[fullLocalPath] = instance;
+                }
+                if (dataPath && !this.instanceMap[dataPath]) {
+                    this.instanceMap[dataPath] = instance;
+                }
+                if (localDataPath && !this.instanceMap[localDataPath]) {
+                    this.instanceMap[localDataPath] = instance;
                 }
             },
-            undefined,
-            undefined,
-            undefined,
             true,
         );
     }
 
-    getComponent(pathArg: string) {
+    getComponent(pathArg: string | string[]) {
+        const path = FormioCore.Utils.getStringFromComponentPath(pathArg);
         // If we don't have an exact path match, compare the final pathname segment with the path argument for each component
         // i.e. getComponent('foo') should return a component at the path 'bar.foo' if it exists
-        if (!this.instanceMap[pathArg]) {
+        if (!this.instanceMap[path]) {
             for (const key in this.instanceMap) {
-                const match = key.match(new RegExp(`\\.${pathArg}$`));
+                const match = key.match(new RegExp(`\\.${path}$`));
                 const lastPathSegment = match ? match[0].slice(1) : '';
-                if (lastPathSegment === pathArg) {
+                if (lastPathSegment === path) {
                     // set a cache for future `getComponent` calls in this lifecycle
-                    this.instanceMap[pathArg] = this.instanceMap[key];
+                    this.instanceMap[path] = this.instanceMap[key];
                     break;
                 }
             }
         }
-        return this.instanceMap[pathArg];
+        return this.instanceMap[path];
     }
 
     get submission() {
